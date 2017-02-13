b5942a974c92f027d22ad83380cf70de3772cc3d
Jonas <jonas.kemper.biz@gmail.com>
2017-02-13 15:55:58 +0100

    initial

---
+++ src/main/java/Wordcount.java
@@ -0,0 +1,62 @@
+/**
+ * Created by jonas on 2/13/17.
+ */
+
+import org.qcri.rheem.api.JavaPlanBuilder;
+import org.qcri.rheem.basic.data.Tuple2;
+import org.qcri.rheem.core.api.Configuration;
+import org.qcri.rheem.core.api.RheemContext;
+import org.qcri.rheem.core.optimizer.cardinality.DefaultCardinalityEstimator;
+import org.qcri.rheem.java.Java;
+import org.qcri.rheem.spark.Spark;
+import java.util.Collection;
+import java.util.Arrays;
+
+public class Wordcount {
+
+
+    public static void main(String[] args){
+
+        // Settings
+        String inputUrl = "file:/Users/jonas/tmp.txt";
+
+        // Get a plan builder.
+        RheemContext rheemContext = new RheemContext(new Configuration())
+                .withPlugin(Java.basicPlugin())
+                .withPlugin(Spark.basicPlugin());
+        JavaPlanBuilder planBuilder = new JavaPlanBuilder(rheemContext)
+                .withJobName(String.format("WordCount (%s)", inputUrl))
+                .withUdfJarOf(Wordcount.class);
+
+        // Start building the RheemPlan.
+        Collection<Tuple2<String, Integer>> wordcounts = planBuilder
+                // Read the text file.
+                .readTextFile(inputUrl).withName("Load file")
+
+                // Split each line by non-word characters.
+                .flatMap(line -> Arrays.asList(line.split("\\W+")))
+                .withSelectivity(10, 100, 0.9)
+                .withName("Split words")
+
+                // Filter empty tokens.
+                .filter(token -> !token.isEmpty())
+                .withSelectivity(0.99, 0.99, 0.99)
+                .withName("Filter empty words")
+
+                // Attach counter to each word.
+                .map(word -> new Tuple2<>(word.toLowerCase(), 1)).withName("To lower case, add counter")
+
+                // Sum up counters for every word.
+                .reduceByKey(
+                        Tuple2::getField0,
+                        (t1, t2) -> new Tuple2<>(t1.getField0(), t1.getField1() + t2.getField1())
+                )
+                .withCardinalityEstimator(new DefaultCardinalityEstimator(0.9, 1, false, in -> Math.round(0.01 * in[0])))
+                .withName("Add counters")
+
+                // Execute the plan and collect the results.
+                .collect();
+
+    }
+
+}

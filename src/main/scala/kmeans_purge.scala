836357ac96c04bd39a99fd3ee25ced5f0eb79fa3
Jonas <jonas.kemper.biz@gmail.com>
2017-03-14 12:50:16 +0100

    before loop operator

--- src/main/scala/kmeans_purge.scala
+++ src/main/scala/kmeans_purge.scala
@@ -87,137 +87,124 @@ object kmeans_purge {
 
 
     // Generate some test data.
-    val inputValues = Array(1, 2)
+    val inputValues = Array(1, 2, 3, 4)
     // Generate some test data.
-    var inputValues2 = planBuilder.loadCollection(Array(3000, 4000))
 
-    // Build and execute a word count RheemPlan.
 
-    val initial_points = planBuilder
-          .readTextFile(inputUrl).withName("Read file")
-          .map { line =>
-            val fields = line.split(",")
-            TaggedPointCounter(fields(0).toDouble, fields(1).toDouble, 0, 1)
-          }
 
-    println(initial_points)
-    val count = initial_points.count.collect()
-    println(count.head)
-    println(initial_points.count)
+    val output = planBuilder.loadCollection(inputValues)
+      .doWhile[Int](condition => condition.size > 2, {pointsx =>
+      val res = pointsx.map(numb => numb + 23)
+      val condition_out = pointsx.map(numb => 2000 + numb)
+      (res.sample(res.count - 1), condition_out)
+    })
 
-    val values = initial_points.withName("Load input values")
-      .doWhile[TaggedPointCounter](points => points.isEmpty, {
-      start =>
-        val foo = start.filter(_ != 42)
-        (start, foo)
-    }).withName("While <= 100")
 
-    println(values.collect())
+    println(output.collect())
 
 
 
 
-
-//    val initial_centroids_list = initialCentroids.collect()
-//
-//    val initial_centroids_map = initialCentroids
-//      .map{ point => point}
-//
-//    println("initialCentroids before:")
-//    println(initialCentroids)
-//    println("initial_centroids_list before:")
-//    println(initial_centroids_list)
-//
-//    val condition = planBuilder.loadCollection(Iterable(1, 2, 3))
-//
-//    val output = initial_centroids_map
-//      .doWhile[Int](condition => condition.max > 100, {pointsx =>
-//      val res = pointsx
-//        .map(point => TaggedPointCounter(point.x * 2, point.y, point.cluster, 0))
-//      val condition_out = condition.map(numb => 2 * numb)
-//      (condition_out, condition_out)
-//
-//      })
-//
-////    val output = initial_centroids_map
-////      .repeat(iterations, {pointsx => pointsx
-////            .map(point => TaggedPointCounter(point.x * 2, point.y, point.cluster, 0))
-////      }).collect()
-//
-//    println("output after:")
-//    println(output.collect())
-
+    //    val initial_centroids_list = initialCentroixds.collect()
+    //
+    //    val initial_centroids_map = initialCentroids
+    //      .map{ point => point}
+    //
+    //    println("initialCentroids before:")
+    //    println(initialCentroids)
+    //    println("initial_centroids_list before:")
+    //    println(initial_centroids_list)
+    //
+    //    val condition = planBuilder.loadCollection(Iterable(1, 2, 3))
+    //
+    //    val output = initial_centroids_map
+    //      .doWhile[Int](condition => condition.max > 100, {pointsx =>
+    //      val res = pointsx
+    //        .map(point => TaggedPointCounter(point.x * 2, point.y, point.cluster, 0))
+    //      val condition_out = condition.map(numb => 2 * numb)
+    //      (condition_out, condition_out)
+    //
+    //      })
+    //
+    ////    val output = initial_centroids_map
+    ////      .repeat(iterations, {pointsx => pointsx
+    ////            .map(point => TaggedPointCounter(point.x * 2, point.y, point.cluster, 0))
+    ////      }).collect()
+    //
+    //    println("output after:")
+    //    println(output.collect())
 
 
-//    val initial_centroid_list = initialCentroids.collect()
-//
-//    val initial_points_list = planBuilder
-//      .readTextFile(inputUrl).withName("Read file")
-//      .map { line =>
-//        val fields = line.split(",")
-//        TaggedPointCounter(fields(0).toDouble, fields(1).toDouble, 0, 1)
-//      }.withName("Create points").collect()
-//
-//    var filtered_centroids = List[TaggedPointCounter]()
-//    var filtered_points = Iterable[TaggedPointCounter]()
-//
-//
-//    println("## initial_centroid_list: ")
-//    println(initial_centroid_list)
-//
-//    println("## initial_points_list: ")
-//    println(initial_points_list)
-//
-//
-//    var i = 0
-//    while (i < iterations) {
-//
-//      var iteration_start_centroids = Iterable[TaggedPointCounter]()
-//      var iteration_start_points = Iterable[TaggedPointCounter]()
-//
-//      if (i == 0) {
-//        iteration_start_centroids = initial_centroid_list
-//        iteration_start_points = initial_points_list
-//      } else {
-//        iteration_start_centroids = filtered_centroids
-//        iteration_start_points = filtered_points
-//      }
-//
-//      val iteration_start_centroids_quanta = planBuilder.loadCollection(iteration_start_centroids)
-//
-//      var points_w_cl_centroid_list = planBuilder.loadCollection(iteration_start_points)
-//        .mapJava(new SelectNearestCentroidForPoint)
-//        .withBroadcast(iteration_start_centroids_quanta, "centroids").withName("Find nearest centroid")
-//        .collect()
-//
-//      val new_centroids_list = planBuilder.loadCollection(points_w_cl_centroid_list)
-//        .reduceByKey(_.cluster, _.add_points(_)).withName("Add up points")
-//        .withCardinalityEstimator(k)
-//        .map(_.average).withName("Average points").collect()
-//
-//      filtered_centroids = List[TaggedPointCounter]()
-//      filtered_points = points_w_cl_centroid_list
-//
-//      for (new_centroid <- new_centroids_list) {
-//        for (old_centroid <- iteration_start_centroids) {
-//          if (new_centroid.cluster == old_centroid.cluster) {
-//            val distance = Math.pow(Math.pow(old_centroid.x - new_centroid.x, 2) + Math.pow(old_centroid.y - new_centroid.y, 2), 0.5)
-//            if (distance < epsilon) {
-//              filtered_points = filtered_points.filter(_.cluster != new_centroid.cluster)
-//            } else {
-//              filtered_centroids ::= new_centroid
-//            }
-//          }
-//        }
-//      }
-//
-//      println("## After iteration " + i)
-//      println("filtered_centroids:")
-//      println(filtered_centroids)
-//      println("filtered_points:")
-//      println(filtered_points)
-//
-//      i += 1
-//    }
+
+    //    val initial_centroid_list = initialCentroids.collect()
+    //
+    //    val initial_points_list = planBuilder
+    //      .readTextFile(inputUrl).withName("Read file")
+    //      .map { line =>
+    //        val fields = line.split(",")
+    //        TaggedPointCounter(fields(0).toDouble, fields(1).toDouble, 0, 1)
+    //      }.withName("Create points").collect()
+    //
+    //    var filtered_centroids = List[TaggedPointCounter]()
+    //    var filtered_points = Iterable[TaggedPointCounter]()
+    //
+    //
+    //    println("## initial_centroid_list: ")
+    //    println(initial_centroid_list)
+    //
+    //    println("## initial_points_list: ")
+    //    println(initial_points_list)
+    //
+    //
+    //    var i = 0
+    //    while (i < iterations) {
+    //
+    //      var iteration_start_centroids = Iterable[TaggedPointCounter]()
+    //      var iteration_start_points = Iterable[TaggedPointCounter]()
+    //
+    //      if (i == 0) {
+    //        iteration_start_centroids = initial_centroid_list
+    //        iteration_start_points = initial_points_list
+    //      } else {
+    //        iteration_start_centroids = filtered_centroids
+    //        iteration_start_points = filtered_points
+    //      }
+    //
+    //      val iteration_start_centroids_quanta = planBuilder.loadCollection(iteration_start_centroids)
+    //
+    //      var points_w_cl_centroid_list = planBuilder.loadCollection(iteration_start_points)
+    //        .mapJava(new SelectNearestCentroidForPoint)
+    //        .withBroadcast(iteration_start_centroids_quanta, "centroids").withName("Find nearest centroid")
+    //        .collect()
+    //
+    //      val new_centroids_list = planBuilder.loadCollection(points_w_cl_centroid_list)
+    //        .reduceByKey(_.cluster, _.add_points(_)).withName("Add up points")
+    //        .withCardinalityEstimator(k)
+    //        .map(_.average).withName("Average points").collect()
+    //
+    //      filtered_centroids = List[TaggedPointCounter]()
+    //      filtered_points = points_w_cl_centroid_list
+    //
+    //      for (new_centroid <- new_centroids_list) {
+    //        for (old_centroid <- iteration_start_centroids) {
+    //          if (new_centroid.cluster == old_centroid.cluster) {
+    //            val distance = Math.pow(Math.pow(old_centroid.x - new_centroid.x, 2) + Math.pow(old_centroid.y - new_centroid.y, 2), 0.5)
+    //            if (distance < epsilon) {
+    //              filtered_points = filtered_points.filter(_.cluster != new_centroid.cluster)
+    //            } else {
+    //              filtered_centroids ::= new_centroid
+    //            }
+    //          }
+    //        }
+    //      }
+    //
+    //      println("## After iteration " + i)
+    //      println("filtered_centroids:")
+    //      println(filtered_centroids)
+    //      println("filtered_points:")
+    //      println(filtered_points)
+    //
+    //      i += 1
+    //    }
   }
 }
